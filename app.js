cd598ec1ecc2059dc7d5b152ff2ec538be4c0f03
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-09-13 21:25:05 +0200

    Basic run command and package.json to have 'npm install' install uglify-js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var h = require('http');
-h.createServer(function(r, s) {
+h = require('http');
+h.createServer(function(request, response) {
 	h.get({
 		host : 'api.twitter.com',
-		path : '/users' + r.url
-	}, function(x) {
-		x.on('data', function(d) {
-			s.write(d.toString('utf8').match(/.+d>(.+?)</)[1] + '\n');
-			s.end();
+		path : '/users' + request.url
+	}, function(apiResponse) {
+		apiResponse.on('data', function(chunk) {
+			response.write(chunk.toString('utf8').match(/.+d>(.+?)</)[1] + '\n');
+			response.end();
 		});
 	});
 }).listen(8080);
